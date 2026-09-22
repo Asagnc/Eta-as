BLACK-BIRTHDAY/Eta

refactor(ui): 通过共享卡片组件统一页面圆角
- 将卡片圆角集中为 24dp，保留静态与交互式卡片的原有行为。
- 同步模型分组首尾与浏览器内容裁剪，避免容器和内容圆角不一致。

app/src/main/kotlin/io/github/mangi/eta/ui/AppearanceSettingsScreen.kt
@@ -32,10 +32,11 @@ import io.github.mangi.eta.data.model.MIN_INTERFACE_SCALE
 import io.github.mangi.eta.data.model.normalizeInterfaceScale
 import io.github.mangi.eta.data.repository.AppearanceSettingsRepository
 import io.github.mangi.eta.ui.app.LocalAppearanceSettings
+import io.github.mangi.eta.ui.components.EtaCard
 import io.github.mangi.eta.ui.components.MiuixDialogActions
 import io.github.mangi.eta.ui.components.MiuixScaffoldPage
+import kotlin.math.roundToInt
 import kotlinx.coroutines.launch
-import top.yukonga.miuix.kmp.basic.Card
 import top.yukonga.miuix.kmp.basic.Slider
 import top.yukonga.miuix.kmp.basic.SliderDefaults
 import top.yukonga.miuix.kmp.basic.SmallTitle
@@ -47,7 +48,6 @@ import top.yukonga.miuix.kmp.preference.OverlayDropdownPreference
 import top.yukonga.miuix.kmp.preference.SwitchPreference
 import top.yukonga.miuix.kmp.theme.MiuixTheme
 import top.yukonga.miuix.kmp.window.WindowDialog
-import kotlin.math.roundToInt
 
 @Composable
 internal fun AppearanceSettingsScreen(onBack: () -> Unit) {
@@ -120,7 +120,7 @@ internal fun AppearanceSettingsScreen(onBack: () -> Unit) {
             SmallTitle(text = stringResource(R.string.appearance_group_color))
         }
         item(key = "appearance_color_card") {
-            Card(modifier = Modifier.padding(horizontal = 12.dp).padding(bottom = 12.dp)) {
+            EtaCard(modifier = Modifier.padding(horizontal = 12.dp).padding(bottom = 12.dp)) {
                 OverlayDropdownPreference(
                     title = stringResource(R.string.appearance_theme_mode),
                     summary = themeModeLabels[appearance.themeMode.ordinal],
@@ -185,7 +185,7 @@ internal fun AppearanceSettingsScreen(onBack: () -> Unit) {
             SmallTitle(text = stringResource(R.string.appearance_group_interface))
         }
         item(key = "appearance_interface_card") {
-            Card(modifier = Modifier.padding(horizontal = 12.dp).padding(bottom = 12.dp)) {
+            EtaCard(modifier = Modifier.padding(horizontal = 12.dp).padding(bottom = 12.dp)) {
                 SwitchPreference(
                     title = stringResource(R.string.appearance_blur),
                     summary = stringResource(R.string.appearance_blur_summary),

app/src/main/kotlin/io/github/mangi/eta/ui/pages/providers/ProviderComponents.kt
@@ -22,9 +22,9 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import io.github.mangi.eta.data.model.CustomProviderSetting
 import io.github.mangi.eta.data.model.ProviderSetting
+import io.github.mangi.eta.ui.components.EtaCard
 import io.github.mangi.eta.ui.components.PreferenceIcon
 import io.github.mangi.eta.ui.components.providerBrandLogoRes as sharedProviderBrandLogoRes
-import top.yukonga.miuix.kmp.basic.Card
 import top.yukonga.miuix.kmp.basic.SmallTitle
 import top.yukonga.miuix.kmp.basic.Text
 import top.yukonga.miuix.kmp.theme.MiuixTheme
@@ -40,7 +40,7 @@ internal fun ProviderSection(
         if (title != null) {
             SmallTitle(title)
         }
-        Card(modifier = Modifier.fillMaxWidth().padding(horizontal = 12.dp)) {
+        EtaCard(modifier = Modifier.fillMaxWidth().padding(horizontal = 12.dp)) {
             content()
         }
     }

app/src/main/kotlin/io/github/mangi/eta/ui/screens/backup/DataBackupScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import io.github.mangi.eta.R
 import io.github.mangi.eta.data.repository.EtaBackupSummary
+import io.github.mangi.eta.ui.components.EtaCard
 import io.github.mangi.eta.ui.components.MiuixDialogActions
 import io.github.mangi.eta.ui.components.MiuixScaffoldPage
 import java.io.InputStream
@@ -34,7 +35,6 @@ import java.util.Locale
 import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.launch
 import top.yukonga.miuix.kmp.basic.BasicComponent
-import top.yukonga.miuix.kmp.basic.Card
 import top.yukonga.miuix.kmp.basic.Icon
 import top.yukonga.miuix.kmp.basic.InfiniteProgressIndicator
 import top.yukonga.miuix.kmp.basic.SmallTitle
@@ -104,7 +104,7 @@ internal fun DataBackupScreen(
         onBack = onBack,
     ) {
         item(key = "warning") {
-            Card(modifier = Modifier.padding(horizontal = 12.dp, vertical = 6.dp)) {
+            EtaCard(modifier = Modifier.padding(horizontal = 12.dp, vertical = 6.dp)) {
                 BasicComponent(
                     title = stringResource(R.string.data_backup_warning_title),
                     summary = stringResource(R.string.data_backup_warning_summary),
@@ -115,7 +115,7 @@ internal fun DataBackupScreen(
             SmallTitle(stringResource(R.string.data_backup_actions))
         }
         item(key = "actions-card") {
-            Card(modifier = Modifier.padding(horizontal = 12.dp).padding(bottom = 12.dp)) {
+            EtaCard(modifier = Modifier.padding(horizontal = 12.dp).padding(bottom = 12.dp)) {
                 ArrowPreference(
                     title = stringResource(R.string.data_backup_export),
                     summary = if (busy) {

app/src/main/kotlin/io/github/mangi/eta/ui/screens/browser/AgentBrowserScreen.kt
@@ -77,13 +77,14 @@ import androidx.core.net.toUri
 import io.github.mangi.eta.R
 import io.github.mangi.eta.agent.browser.AgentBrowserSession
 import io.github.mangi.eta.agent.browser.BrowserSessionSnapshot
+import io.github.mangi.eta.ui.components.EtaCard
+import io.github.mangi.eta.ui.components.EtaCardDefaults
 import io.github.mangi.eta.ui.components.MiuixDialogActions
 import io.github.mangi.eta.ui.components.StatusError
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import top.yukonga.miuix.kmp.basic.ButtonDefaults
-import top.yukonga.miuix.kmp.basic.Card
 import top.yukonga.miuix.kmp.basic.CardDefaults
 import top.yukonga.miuix.kmp.basic.Icon
 import top.yukonga.miuix.kmp.basic.IconButton
@@ -280,7 +281,7 @@ private fun BrowserWindow(
     onReset: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Card(
+    EtaCard(
         modifier = modifier,
         insideMargin = PaddingValues(0.dp),
         colors = CardDefaults.defaultColors(
@@ -311,8 +312,8 @@ private fun BrowserWindow(
                 // 普通 clip 走 clipToOutline，硬件裁剪对 WebView 安全。
                 .clip(
                     RoundedCornerShape(
-                        bottomStart = CardDefaults.CornerRadius,
-                        bottomEnd = CardDefaults.CornerRadius,
+                        bottomStart = EtaCardDefaults.CornerRadius,
+                        bottomEnd = EtaCardDefaults.CornerRadius,
                     )
                 ),
         ) {
@@ -518,7 +519,7 @@ private fun ColumnScope.BrowserStatusBanner(snapshot: BrowserSessionSnapshot) {
         enter = fadeIn() + expandVertically(),
         exit = fadeOut() + shrinkVertically(),
     ) {
-        Card(
+        EtaCard(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(bottom = 10.dp),

app/src/main/kotlin/io/github/mangi/eta/ui/screens/characters/CharacterEditorScreen.kt
@@ -17,9 +17,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import io.github.mangi.eta.ui.app.CharacterLibraryStore
+import io.github.mangi.eta.ui.components.EtaCard
 import io.github.mangi.eta.ui.components.MiuixScaffoldPage
 import top.yukonga.miuix.kmp.basic.ButtonDefaults
-import top.yukonga.miuix.kmp.basic.Card
 import top.yukonga.miuix.kmp.basic.Icon
 import top.yukonga.miuix.kmp.basic.IconButton
 import top.yukonga.miuix.kmp.basic.SmallTitle
@@ -103,7 +103,7 @@ internal fun CharacterEditorScreen(
             )
         }
         item(key = "advanced") {
-            Card(modifier = Modifier.padding(horizontal = CharacterCardPadding, vertical = 8.dp)) {
+            EtaCard(modifier = Modifier.padding(horizontal = CharacterCardPadding, vertical = 8.dp)) {
                 ArrowPreference(
                     title = "高级设置",
                     summary = "性格、背景、示例对话、提示词、作者信息与世界书",

app/src/main/kotlin/io/github/mangi/eta/ui/screens/permissions/PermissionHealthScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import io.github.mangi.eta.R
+import io.github.mangi.eta.ui.components.EtaCard
 import io.github.mangi.eta.ui.components.MiuixScaffoldPage
 import io.github.mangi.eta.ui.components.PreferenceIcon
 import io.github.mangi.eta.ui.components.color
@@ -28,7 +29,6 @@ import io.github.mangi.eta.ui.components.label
 import io.github.mangi.eta.ui.model.PermissionHealthAction
 import io.github.mangi.eta.ui.model.PermissionHealthItemUi
 import io.github.mangi.eta.ui.model.PermissionHealthUiState
-import top.yukonga.miuix.kmp.basic.Card
 import top.yukonga.miuix.kmp.basic.SmallTitle
 import top.yukonga.miuix.kmp.basic.Text
 import top.yukonga.miuix.kmp.preference.ArrowPreference
@@ -49,7 +49,7 @@ fun PermissionHealthScreen(
             SmallTitle(stringResource(R.string.ui_permissions_and_status_35f368))
         }
         item(key = "card") {
-            Card(modifier = Modifier.padding(horizontal = 12.dp)) {
+            EtaCard(modifier = Modifier.padding(horizontal = 12.dp)) {
                 state.items.forEach { item ->
                     PermissionItemRow(
                         item = item,

app/src/main/kotlin/io/github/mangi/eta/ui/screens/terminal/LinuxEnvironmentSections.kt
@@ -14,9 +14,9 @@ import androidx.compose.ui.unit.dp
 import io.github.mangi.eta.R
 import io.github.mangi.eta.agent.terminal.LinuxDistribution
 import io.github.mangi.eta.agent.terminal.LinuxExecutionBackend
+import io.github.mangi.eta.ui.components.EtaCard
 import top.yukonga.miuix.kmp.basic.BasicComponent
 import top.yukonga.miuix.kmp.basic.ButtonDefaults
-import top.yukonga.miuix.kmp.basic.Card
 import top.yukonga.miuix.kmp.basic.DropdownItem
 import top.yukonga.miuix.kmp.basic.InfiniteProgressIndicator
 import top.yukonga.miuix.kmp.basic.Text
@@ -35,7 +35,7 @@ internal fun LinuxEnvironmentStatusCard(
     actionEnabled: Boolean,
     onAction: () -> Unit,
 ) {
-    Card(
+    EtaCard(
         modifier = Modifier.padding(horizontal = 12.dp, vertical = 6.dp),
         insideMargin = PaddingValues(16.dp),
     ) {
@@ -91,7 +91,7 @@ internal fun LinuxEnvironmentConfiguration(
 ) {
     val distributions = LinuxDistribution.entries
     val backends = listOf(LinuxExecutionBackend.PROOT, LinuxExecutionBackend.CHROOT)
-    Card(modifier = Modifier.padding(horizontal = 12.dp, vertical = 6.dp)) {
+    EtaCard(modifier = Modifier.padding(horizontal = 12.dp, vertical = 6.dp)) {
         WindowSpinnerPreference(
             title = stringResource(R.string.linux_distribution_title),
             items = distributions.map {

Asagnc/Eta-as

(cherry picked from commit 924824d441a0f896aeba3f9797856909e76c98c4)

app/src/main/kotlin/io/github/asagnc/sta/ui/AppearanceSettingsScreen.kt
@@ -34,8 +34,9 @@ import io.github.asagnc.sta.data.repository.AppearanceSettingsRepository
 import io.github.asagnc.sta.ui.app.LocalAppearanceSettings
 import io.github.asagnc.sta.ui.components.MiuixDialogActions
 import io.github.asagnc.sta.ui.components.MiuixScaffoldPage
+import io.github.asagnc.sta.ui.components.StaCard
+import kotlin.math.roundToInt
 import kotlinx.coroutines.launch
-import top.yukonga.miuix.kmp.basic.Card
 import top.yukonga.miuix.kmp.basic.Slider
 import top.yukonga.miuix.kmp.basic.SliderDefaults
 import top.yukonga.miuix.kmp.basic.SmallTitle
@@ -47,7 +48,6 @@ import top.yukonga.miuix.kmp.preference.OverlayDropdownPreference
 import top.yukonga.miuix.kmp.preference.SwitchPreference
 import top.yukonga.miuix.kmp.theme.MiuixTheme
 import top.yukonga.miuix.kmp.window.WindowDialog
-import kotlin.math.roundToInt
 
 @Composable
 internal fun AppearanceSettingsScreen(onBack: () -> Unit) {
@@ -120,7 +120,7 @@ internal fun AppearanceSettingsScreen(onBack: () -> Unit) {
             SmallTitle(text = stringResource(R.string.appearance_group_color))
         }
         item(key = "appearance_color_card") {
-            Card(modifier = Modifier.padding(horizontal = 12.dp).padding(bottom = 12.dp)) {
+            StaCard(modifier = Modifier.padding(horizontal = 12.dp).padding(bottom = 12.dp)) {
                 OverlayDropdownPreference(
                     title = stringResource(R.string.appearance_theme_mode),
                     summary = themeModeLabels[appearance.themeMode.ordinal],
@@ -185,7 +185,7 @@ internal fun AppearanceSettingsScreen(onBack: () -> Unit) {
             SmallTitle(text = stringResource(R.string.appearance_group_interface))
         }
         item(key = "appearance_interface_card") {
-            Card(modifier = Modifier.padding(horizontal = 12.dp).padding(bottom = 12.dp)) {
+            StaCard(modifier = Modifier.padding(horizontal = 12.dp).padding(bottom = 12.dp)) {
                 SwitchPreference(
                     title = stringResource(R.string.appearance_blur),
                     summary = stringResource(R.string.appearance_blur_summary),

app/src/main/kotlin/io/github/asagnc/sta/ui/pages/providers/ProviderComponents.kt
@@ -25,6 +25,7 @@ import io.github.asagnc.sta.data.model.ProviderSetting
 import io.github.asagnc.sta.ui.components.PreferenceIcon
 import io.github.asagnc.sta.ui.components.providerBrandLogoRes as sharedProviderBrandLogoRes
 import top.yukonga.miuix.kmp.basic.Card
+import io.github.asagnc.sta.ui.components.StaCard
 import top.yukonga.miuix.kmp.basic.SmallTitle
 import top.yukonga.miuix.kmp.basic.Text
 import top.yukonga.miuix.kmp.theme.MiuixTheme
@@ -40,7 +41,7 @@ internal fun ProviderSection(
         if (title != null) {
             SmallTitle(title)
         }
-        Card(modifier = Modifier.fillMaxWidth().padding(horizontal = 12.dp)) {
+        StaCard(modifier = Modifier.fillMaxWidth().padding(horizontal = 12.dp)) {
             content()
         }
     }

app/src/main/kotlin/io/github/asagnc/sta/ui/screens/backup/DataBackupScreen.kt
@@ -26,6 +26,7 @@ import io.github.asagnc.sta.R
 import io.github.asagnc.sta.data.repository.StaBackupSummary
 import io.github.asagnc.sta.ui.components.MiuixDialogActions
 import io.github.asagnc.sta.ui.components.MiuixScaffoldPage
+import io.github.asagnc.sta.ui.components.StaCard
 import java.io.InputStream
 import java.io.OutputStream
 import java.text.SimpleDateFormat
@@ -34,7 +35,6 @@ import java.util.Locale
 import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.launch
 import top.yukonga.miuix.kmp.basic.BasicComponent
-import top.yukonga.miuix.kmp.basic.Card
 import top.yukonga.miuix.kmp.basic.Icon
 import top.yukonga.miuix.kmp.basic.InfiniteProgressIndicator
 import top.yukonga.miuix.kmp.basic.SmallTitle
@@ -104,7 +104,7 @@ internal fun DataBackupScreen(
         onBack = onBack,
     ) {
         item(key = "warning") {
-            Card(modifier = Modifier.padding(horizontal = 12.dp, vertical = 6.dp)) {
+            StaCard(modifier = Modifier.padding(horizontal = 12.dp, vertical = 6.dp)) {
                 BasicComponent(
                     title = stringResource(R.string.data_backup_warning_title),
                     summary = stringResource(R.string.data_backup_warning_summary),
@@ -115,7 +115,7 @@ internal fun DataBackupScreen(
             SmallTitle(stringResource(R.string.data_backup_actions))
         }
         item(key = "actions-card") {
-            Card(modifier = Modifier.padding(horizontal = 12.dp).padding(bottom = 12.dp)) {
+            StaCard(modifier = Modifier.padding(horizontal = 12.dp).padding(bottom = 12.dp)) {
                 ArrowPreference(
                     title = stringResource(R.string.data_backup_export),
                     summary = if (busy) {

app/src/main/kotlin/io/github/asagnc/sta/ui/screens/browser/AgentBrowserScreen.kt
@@ -80,11 +80,12 @@ import io.github.asagnc.sta.agent.browser.AgentBrowserSession
 import io.github.asagnc.sta.agent.browser.BrowserSessionSnapshot
 import io.github.asagnc.sta.ui.components.MiuixDialogActions
 import io.github.asagnc.sta.ui.components.StatusError
+import io.github.asagnc.sta.ui.components.StaCard
+import io.github.asagnc.sta.ui.components.StaCardDefaults
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import top.yukonga.miuix.kmp.basic.ButtonDefaults
-import top.yukonga.miuix.kmp.basic.Card
 import top.yukonga.miuix.kmp.basic.CardDefaults
 import top.yukonga.miuix.kmp.basic.Icon
 import top.yukonga.miuix.kmp.basic.IconButton
@@ -325,7 +326,7 @@ private fun BrowserWindow(
     onProxy: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Card(
+    StaCard(
         modifier = modifier,
         insideMargin = PaddingValues(0.dp),
         colors = CardDefaults.defaultColors(
@@ -357,8 +358,8 @@ private fun BrowserWindow(
                 // 普通 clip 走 clipToOutline，硬件裁剪对 WebView 安全。
                 .clip(
                     RoundedCornerShape(
-                        bottomStart = CardDefaults.CornerRadius,
-                        bottomEnd = CardDefaults.CornerRadius,
+                        bottomStart = StaCardDefaults.CornerRadius,
+                        bottomEnd = StaCardDefaults.CornerRadius,
                     )
                 ),
         ) {
@@ -581,7 +582,7 @@ private fun ColumnScope.BrowserStatusBanner(snapshot: BrowserSessionSnapshot) {
         enter = fadeIn() + expandVertically(),
         exit = fadeOut() + shrinkVertically(),
     ) {
-        Card(
+        StaCard(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(bottom = 10.dp),

app/src/main/kotlin/io/github/asagnc/sta/ui/screens/characters/CharacterEditorScreen.kt
@@ -18,8 +18,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import io.github.asagnc.sta.ui.app.CharacterLibraryStore
 import io.github.asagnc.sta.ui.components.MiuixScaffoldPage
+import io.github.asagnc.sta.ui.components.StaCard
 import top.yukonga.miuix.kmp.basic.ButtonDefaults
-import top.yukonga.miuix.kmp.basic.Card
 import top.yukonga.miuix.kmp.basic.Icon
 import top.yukonga.miuix.kmp.basic.IconButton
 import top.yukonga.miuix.kmp.basic.SmallTitle
@@ -103,7 +103,7 @@ internal fun CharacterEditorScreen(
             )
         }
         item(key = "advanced") {
-            Card(modifier = Modifier.padding(horizontal = CharacterCardPadding, vertical = 8.dp)) {
+            StaCard(modifier = Modifier.padding(horizontal = CharacterCardPadding, vertical = 8.dp)) {
                 ArrowPreference(
                     title = "高级设置",
                     summary = "性格、背景、示例对话、提示词、作者信息与世界书",

app/src/main/kotlin/io/github/asagnc/sta/ui/screens/permissions/PermissionHealthScreen.kt
@@ -29,6 +29,7 @@ import io.github.asagnc.sta.ui.model.PermissionHealthAction
 import io.github.asagnc.sta.ui.model.PermissionHealthItemUi
 import io.github.asagnc.sta.ui.model.PermissionHealthUiState
 import top.yukonga.miuix.kmp.basic.Card
+import io.github.asagnc.sta.ui.components.StaCard
 import top.yukonga.miuix.kmp.basic.SmallTitle
 import top.yukonga.miuix.kmp.basic.Text
 import top.yukonga.miuix.kmp.preference.ArrowPreference
@@ -49,7 +50,7 @@ fun PermissionHealthScreen(
             SmallTitle(stringResource(R.string.ui_permissions_and_status_35f368))
         }
         item(key = "card") {
-            Card(modifier = Modifier.padding(horizontal = 12.dp)) {
+            StaCard(modifier = Modifier.padding(horizontal = 12.dp)) {
                 state.items.forEach { item ->
                     PermissionItemRow(
                         item = item,

app/src/main/kotlin/io/github/asagnc/sta/ui/screens/terminal/LinuxEnvironmentSections.kt
@@ -14,9 +14,9 @@ import androidx.compose.ui.unit.dp
 import io.github.asagnc.sta.R
 import io.github.asagnc.sta.agent.terminal.LinuxDistribution
 import io.github.asagnc.sta.agent.terminal.LinuxExecutionBackend
+import io.github.asagnc.sta.ui.components.StaCard
 import top.yukonga.miuix.kmp.basic.BasicComponent
 import top.yukonga.miuix.kmp.basic.ButtonDefaults
-import top.yukonga.miuix.kmp.basic.Card
 import top.yukonga.miuix.kmp.basic.DropdownItem
 import top.yukonga.miuix.kmp.basic.InfiniteProgressIndicator
 import top.yukonga.miuix.kmp.basic.Text
@@ -35,7 +35,7 @@ internal fun LinuxEnvironmentStatusCard(
     actionEnabled: Boolean,
     onAction: () -> Unit,
 ) {
-    Card(
+    StaCard(
         modifier = Modifier.padding(horizontal = 12.dp, vertical = 6.dp),
         insideMargin = PaddingValues(16.dp),
     ) {
@@ -91,7 +91,7 @@ internal fun LinuxEnvironmentConfiguration(
 ) {
     val distributions = LinuxDistribution.entries
     val backends = listOf(LinuxExecutionBackend.PROOT, LinuxExecutionBackend.CHROOT)
-    Card(modifier = Modifier.padding(horizontal = 12.dp, vertical = 6.dp)) {
+    StaCard(modifier = Modifier.padding(horizontal = 12.dp, vertical = 6.dp)) {
         WindowSpinnerPreference(
             title = stringResource(R.string.linux_distribution_title),
             items = distributions.map {