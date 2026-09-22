BLACK-BIRTHDAY/Eta

refactor(ui): 将会话侧栏重构为并排推移式抽屉
- 原实现将会话列表压在聊天页下方，只移动聊天层并对侧栏叠加遮罩，且横向手势仅挂在聊天层；改为同一容器内并排推移，仅压暗非当前操作的聊天区。
- 原搜索区与底部工具条覆盖在全高列表上，依赖高度回填、毛玻璃和分割线遮挡滚动内容；改为上下固定占位、中间独立裁剪滚动，并精简底部为纯图标入口。
- 移除页面圆角、投影与叠层装饰，拆分侧栏内容、位移计算和状态同步职责，降低绘制复杂度。
- 原状态同步只观察 settledValue，动画中断后回到原锚点会漏报；补齐运动结束后的同步及回归测试，避免菜单无法再次展开。

app/src/main/kotlin/io/github/mangi/eta/ui/components/ConversationPaneMotion.kt
@@ -1,0 +1,17 @@
+package io.github.mangi.eta.ui.components
+
+internal data class ConversationPaneMotion(
+    val progress: Float,
+    val foregroundOffset: Float,
+    val panelOffset: Float,
+)
+
+internal fun conversationPaneMotion(offset: Float, paneWidth: Float): ConversationPaneMotion {
+    val width = paneWidth.takeIf { it.isFinite() && it > 0f } ?: 0f
+    val displacement = if (offset.isFinite()) offset.coerceIn(0f, width) else 0f
+    return ConversationPaneMotion(
+        progress = if (width > 0f) displacement / width else 0f,
+        foregroundOffset = displacement,
+        panelOffset = displacement - width,
+    )
+}

app/src/main/kotlin/io/github/mangi/eta/ui/components/ConversationPaneState.kt
@@ -1,0 +1,26 @@
+package io.github.mangi.eta.ui.components
+
+import androidx.compose.foundation.gestures.AnchoredDraggableState
+import androidx.compose.runtime.snapshotFlow
+import kotlin.math.abs
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.filterNotNull
+
+internal enum class ConversationPaneAnchor {
+    Closed,
+    Open,
+}
+
+internal fun conversationPaneSettledValues(
+    state: AnchoredDraggableState<ConversationPaneAnchor>,
+    isDragging: () -> Boolean,
+): Flow<ConversationPaneAnchor> = snapshotFlow {
+    val anchor = state.settledValue
+    val position = state.anchors.positionOf(anchor)
+    if (!isDragging() && !state.isAnimationRunning && abs(state.offset - position) < 0.5f) {
+        anchor
+    } else {
+        null
+    }
+    // 必须先让 snapshotFlow 观察到运动中的 null，再过滤；回到原锚点也要重新同步。
+}.filterNotNull()

app/src/test/kotlin/io/github/mangi/eta/ui/components/ConversationPaneMotionTest.kt
@@ -1,0 +1,63 @@
+package io.github.mangi.eta.ui.components
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class ConversationPaneMotionTest {
+    @Test
+    fun closedAndOpenPositionsMatchTheViewportEdges() {
+        val closed = conversationPaneMotion(0f, 320f)
+        assertEquals(0f, closed.foregroundOffset, 0f)
+        assertEquals(-320f, closed.panelOffset, 0f)
+        assertEquals(0f, closed.progress, 0f)
+
+        val open = conversationPaneMotion(320f, 320f)
+        assertEquals(320f, open.foregroundOffset, 0f)
+        assertEquals(0f, open.panelOffset, 0f)
+        assertEquals(1f, open.progress, 0f)
+    }
+
+    @Test
+    fun panesStayAdjacentWithoutOverlapOrGapsThroughoutEitherDirection() {
+        for (width in listOf(280f, 320f, 340f)) {
+            val positions = listOf(0f, 0.1f, 0.5f, 0.6f, 40f, width / 2f, width)
+            val opening = positions.map { conversationPaneMotion(it, width) }
+            opening.forEach { motion ->
+                assertEquals(motion.foregroundOffset, motion.panelOffset + width, 0.0001f)
+            }
+            assertTrue(opening[3].progress < 0.01f)
+            opening.zipWithNext().forEach { (before, after) ->
+                assertTrue(after.progress >= before.progress)
+                assertEquals(
+                    after.foregroundOffset - before.foregroundOffset,
+                    after.panelOffset - before.panelOffset,
+                    0.0001f,
+                )
+            }
+            val closing = positions.reversed().map { conversationPaneMotion(it, width) }
+            assertEquals(opening, closing.reversed())
+        }
+    }
+
+    @Test
+    fun overshootAndUninitializedOffsetsCannotProduceInvalidLayers() {
+        assertEquals(conversationPaneMotion(0f, 320f), conversationPaneMotion(-20f, 320f))
+        assertEquals(conversationPaneMotion(320f, 320f), conversationPaneMotion(400f, 320f))
+        assertEquals(conversationPaneMotion(0f, 320f), conversationPaneMotion(Float.NaN, 320f))
+        for (width in listOf(0f, -1f, Float.NaN, Float.POSITIVE_INFINITY)) {
+            val motion = conversationPaneMotion(40f, width)
+            assertEquals(0f, motion.progress, 0f)
+            assertEquals(0f, motion.foregroundOffset, 0f)
+            assertTrue(motion.panelOffset.isFinite())
+        }
+    }
+
+    @Test
+    fun resizedWindowsKeepTheSameProgressAtEquivalentDragFractions() {
+        val compact = conversationPaneMotion(140f, 280f)
+        val wide = conversationPaneMotion(170f, 340f)
+        assertEquals(compact.progress, wide.progress, 0f)
+        assertEquals(compact.panelOffset / 280f, wide.panelOffset / 340f, 0.00001f)
+    }
+}

app/src/test/kotlin/io/github/mangi/eta/ui/components/ConversationPaneStateTest.kt
@@ -1,0 +1,105 @@
+package io.github.mangi.eta.ui.components
+
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.MutatePriority
+import androidx.compose.foundation.gestures.AnchoredDraggableState
+import androidx.compose.foundation.gestures.DraggableAnchors
+import androidx.compose.foundation.gestures.animateTo
+import androidx.compose.foundation.gestures.snapTo
+import androidx.compose.runtime.MonotonicFrameClock
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.snapshots.Snapshot
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeout
+import kotlinx.coroutines.yield
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class ConversationPaneStateTest {
+    @Test
+    fun interruptedOpeningResynchronizesClosedAndCanOpenAgain() = verifyInterruptedTransition(
+        initial = ConversationPaneAnchor.Closed,
+        target = ConversationPaneAnchor.Open,
+    )
+
+    @Test
+    fun interruptedClosingResynchronizesOpenAndCanCloseAgain() = verifyInterruptedTransition(
+        initial = ConversationPaneAnchor.Open,
+        target = ConversationPaneAnchor.Closed,
+    )
+
+    private fun verifyInterruptedTransition(initial: ConversationPaneAnchor, target: ConversationPaneAnchor) = runBlocking {
+        withTimeout(5_000) {
+            val anchors = DraggableAnchors {
+                ConversationPaneAnchor.Closed at 0f
+                ConversationPaneAnchor.Open at 320f
+            }
+            val state = AnchoredDraggableState(initialValue = initial, anchors = anchors)
+            val dragging = mutableStateOf(false)
+            val observed = mutableListOf<ConversationPaneAnchor>()
+            var visible = initial == ConversationPaneAnchor.Open
+            val observer = launch(start = CoroutineStart.UNDISPATCHED) {
+                conversationPaneSettledValues(state) { dragging.value }.collect {
+                    observed += it
+                    visible = it == ConversationPaneAnchor.Open
+                }
+            }
+            val clock = TestFrameClock()
+            visible = target == ConversationPaneAnchor.Open
+            val animation = launch(clock, start = CoroutineStart.UNDISPATCHED) {
+                state.animateTo(target, tween(1000, easing = LinearEasing))
+            }
+            try {
+                clock.send(0L)
+                yield()
+                clock.send(100_000_000L)
+                yield()
+                flushSnapshots()
+                assertTrue(state.offset > 0f && state.offset < 320f)
+
+                dragging.value = true
+                flushSnapshots()
+                state.anchoredDrag(dragPriority = MutatePriority.UserInput) {
+                    dragTo(anchors.positionOf(initial))
+                }
+                animation.join()
+                flushSnapshots()
+                assertTrue(animation.isCancelled)
+                assertEquals(initial, state.settledValue)
+                assertEquals(listOf(initial), observed)
+                assertEquals(target == ConversationPaneAnchor.Open, visible)
+
+                dragging.value = false
+                flushSnapshots()
+                assertEquals(listOf(initial, initial), observed)
+                assertEquals(initial == ConversationPaneAnchor.Open, visible)
+
+                visible = target == ConversationPaneAnchor.Open
+                state.snapTo(target)
+                flushSnapshots()
+                assertEquals(listOf(initial, initial, target), observed)
+                assertEquals(target == ConversationPaneAnchor.Open, visible)
+            } finally {
+                animation.cancelAndJoin()
+                observer.cancelAndJoin()
+            }
+        }
+    }
+
+    private suspend fun flushSnapshots() {
+        Snapshot.sendApplyNotifications()
+        yield()
+    }
+
+    private class TestFrameClock : MonotonicFrameClock {
+        private val frames = Channel<Long>(Channel.UNLIMITED)
+        fun send(time: Long) { check(frames.trySend(time).isSuccess) }
+        override suspend fun <R> withFrameNanos(onFrame: (Long) -> R): R = onFrame(frames.receive())
+    }
+}

Asagnc/Eta-as

(cherry picked from commit fa2c468c289be3f17c73fc8bed922a624eda42a7)

app/src/main/kotlin/io/github/asagnc/sta/ui/components/ConversationPaneMotion.kt
@@ -1,0 +1,17 @@
+package io.github.asagnc.sta.ui.components
+
+internal data class ConversationPaneMotion(
+    val progress: Float,
+    val foregroundOffset: Float,
+    val panelOffset: Float,
+)
+
+internal fun conversationPaneMotion(offset: Float, paneWidth: Float): ConversationPaneMotion {
+    val width = paneWidth.takeIf { it.isFinite() && it > 0f } ?: 0f
+    val displacement = if (offset.isFinite()) offset.coerceIn(0f, width) else 0f
+    return ConversationPaneMotion(
+        progress = if (width > 0f) displacement / width else 0f,
+        foregroundOffset = displacement,
+        panelOffset = displacement - width,
+    )
+}

app/src/main/kotlin/io/github/asagnc/sta/ui/components/ConversationPaneState.kt
@@ -1,0 +1,26 @@
+package io.github.asagnc.sta.ui.components
+
+import androidx.compose.foundation.gestures.AnchoredDraggableState
+import androidx.compose.runtime.snapshotFlow
+import kotlin.math.abs
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.filterNotNull
+
+internal enum class ConversationPaneAnchor {
+    Closed,
+    Open,
+}
+
+internal fun conversationPaneSettledValues(
+    state: AnchoredDraggableState<ConversationPaneAnchor>,
+    isDragging: () -> Boolean,
+): Flow<ConversationPaneAnchor> = snapshotFlow {
+    val anchor = state.settledValue
+    val position = state.anchors.positionOf(anchor)
+    if (!isDragging() && !state.isAnimationRunning && abs(state.offset - position) < 0.5f) {
+        anchor
+    } else {
+        null
+    }
+    // 必须先让 snapshotFlow 观察到运动中的 null，再过滤；回到原锚点也要重新同步。
+}.filterNotNull()

app/src/test/kotlin/io/github/asagnc/sta/ui/components/ConversationPaneMotionTest.kt
@@ -1,0 +1,63 @@
+package io.github.asagnc.sta.ui.components
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class ConversationPaneMotionTest {
+    @Test
+    fun closedAndOpenPositionsMatchTheViewportEdges() {
+        val closed = conversationPaneMotion(0f, 320f)
+        assertEquals(0f, closed.foregroundOffset, 0f)
+        assertEquals(-320f, closed.panelOffset, 0f)
+        assertEquals(0f, closed.progress, 0f)
+
+        val open = conversationPaneMotion(320f, 320f)
+        assertEquals(320f, open.foregroundOffset, 0f)
+        assertEquals(0f, open.panelOffset, 0f)
+        assertEquals(1f, open.progress, 0f)
+    }
+
+    @Test
+    fun panesStayAdjacentWithoutOverlapOrGapsThroughoutEitherDirection() {
+        for (width in listOf(280f, 320f, 340f)) {
+            val positions = listOf(0f, 0.1f, 0.5f, 0.6f, 40f, width / 2f, width)
+            val opening = positions.map { conversationPaneMotion(it, width) }
+            opening.forEach { motion ->
+                assertEquals(motion.foregroundOffset, motion.panelOffset + width, 0.0001f)
+            }
+            assertTrue(opening[3].progress < 0.01f)
+            opening.zipWithNext().forEach { (before, after) ->
+                assertTrue(after.progress >= before.progress)
+                assertEquals(
+                    after.foregroundOffset - before.foregroundOffset,
+                    after.panelOffset - before.panelOffset,
+                    0.0001f,
+                )
+            }
+            val closing = positions.reversed().map { conversationPaneMotion(it, width) }
+            assertEquals(opening, closing.reversed())
+        }
+    }
+
+    @Test
+    fun overshootAndUninitializedOffsetsCannotProduceInvalidLayers() {
+        assertEquals(conversationPaneMotion(0f, 320f), conversationPaneMotion(-20f, 320f))
+        assertEquals(conversationPaneMotion(320f, 320f), conversationPaneMotion(400f, 320f))
+        assertEquals(conversationPaneMotion(0f, 320f), conversationPaneMotion(Float.NaN, 320f))
+        for (width in listOf(0f, -1f, Float.NaN, Float.POSITIVE_INFINITY)) {
+            val motion = conversationPaneMotion(40f, width)
+            assertEquals(0f, motion.progress, 0f)
+            assertEquals(0f, motion.foregroundOffset, 0f)
+            assertTrue(motion.panelOffset.isFinite())
+        }
+    }
+
+    @Test
+    fun resizedWindowsKeepTheSameProgressAtEquivalentDragFractions() {
+        val compact = conversationPaneMotion(140f, 280f)
+        val wide = conversationPaneMotion(170f, 340f)
+        assertEquals(compact.progress, wide.progress, 0f)
+        assertEquals(compact.panelOffset / 280f, wide.panelOffset / 340f, 0.00001f)
+    }
+}

app/src/test/kotlin/io/github/asagnc/sta/ui/components/ConversationPaneStateTest.kt
@@ -1,0 +1,105 @@
+package io.github.asagnc.sta.ui.components
+
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.MutatePriority
+import androidx.compose.foundation.gestures.AnchoredDraggableState
+import androidx.compose.foundation.gestures.DraggableAnchors
+import androidx.compose.foundation.gestures.animateTo
+import androidx.compose.foundation.gestures.snapTo
+import androidx.compose.runtime.MonotonicFrameClock
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.snapshots.Snapshot
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeout
+import kotlinx.coroutines.yield
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class ConversationPaneStateTest {
+    @Test
+    fun interruptedOpeningResynchronizesClosedAndCanOpenAgain() = verifyInterruptedTransition(
+        initial = ConversationPaneAnchor.Closed,
+        target = ConversationPaneAnchor.Open,
+    )
+
+    @Test
+    fun interruptedClosingResynchronizesOpenAndCanCloseAgain() = verifyInterruptedTransition(
+        initial = ConversationPaneAnchor.Open,
+        target = ConversationPaneAnchor.Closed,
+    )
+
+    private fun verifyInterruptedTransition(initial: ConversationPaneAnchor, target: ConversationPaneAnchor) = runBlocking {
+        withTimeout(5_000) {
+            val anchors = DraggableAnchors {
+                ConversationPaneAnchor.Closed at 0f
+                ConversationPaneAnchor.Open at 320f
+            }
+            val state = AnchoredDraggableState(initialValue = initial, anchors = anchors)
+            val dragging = mutableStateOf(false)
+            val observed = mutableListOf<ConversationPaneAnchor>()
+            var visible = initial == ConversationPaneAnchor.Open
+            val observer = launch(start = CoroutineStart.UNDISPATCHED) {
+                conversationPaneSettledValues(state) { dragging.value }.collect {
+                    observed += it
+                    visible = it == ConversationPaneAnchor.Open
+                }
+            }
+            val clock = TestFrameClock()
+            visible = target == ConversationPaneAnchor.Open
+            val animation = launch(clock, start = CoroutineStart.UNDISPATCHED) {
+                state.animateTo(target, tween(1000, easing = LinearEasing))
+            }
+            try {
+                clock.send(0L)
+                yield()
+                clock.send(100_000_000L)
+                yield()
+                flushSnapshots()
+                assertTrue(state.offset > 0f && state.offset < 320f)
+
+                dragging.value = true
+                flushSnapshots()
+                state.anchoredDrag(dragPriority = MutatePriority.UserInput) {
+                    dragTo(anchors.positionOf(initial))
+                }
+                animation.join()
+                flushSnapshots()
+                assertTrue(animation.isCancelled)
+                assertEquals(initial, state.settledValue)
+                assertEquals(listOf(initial), observed)
+                assertEquals(target == ConversationPaneAnchor.Open, visible)
+
+                dragging.value = false
+                flushSnapshots()
+                assertEquals(listOf(initial, initial), observed)
+                assertEquals(initial == ConversationPaneAnchor.Open, visible)
+
+                visible = target == ConversationPaneAnchor.Open
+                state.snapTo(target)
+                flushSnapshots()
+                assertEquals(listOf(initial, initial, target), observed)
+                assertEquals(target == ConversationPaneAnchor.Open, visible)
+            } finally {
+                animation.cancelAndJoin()
+                observer.cancelAndJoin()
+            }
+        }
+    }
+
+    private suspend fun flushSnapshots() {
+        Snapshot.sendApplyNotifications()
+        yield()
+    }
+
+    private class TestFrameClock : MonotonicFrameClock {
+        private val frames = Channel<Long>(Channel.UNLIMITED)
+        fun send(time: Long) { check(frames.trySend(time).isSuccess) }
+        override suspend fun <R> withFrameNanos(onFrame: (Long) -> R): R = onFrame(frames.receive())
+    }
+}