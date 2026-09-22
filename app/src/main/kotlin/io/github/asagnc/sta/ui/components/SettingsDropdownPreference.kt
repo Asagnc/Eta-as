BLACK-BIRTHDAY/Eta

feat(ui): 统一设置首页图标配色与列表布局
- 使用独立行布局统一设置项高度与对齐，保留长文本自适应及 Miuix 开关和菜单交互。
- 集中管理分类图标颜色、卡片样式和分割线，沿用默认字重并兼容深色与 Monet 主题。

app/src/main/kotlin/io/github/mangi/eta/ui/components/SettingsDropdownPreference.kt
@@ -1,0 +1,94 @@
+package io.github.mangi.eta.ui.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.ui.platform.LocalHapticFeedback
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import top.yukonga.miuix.kmp.basic.DropdownArrowEndAction
+import top.yukonga.miuix.kmp.basic.DropdownDefaults
+import top.yukonga.miuix.kmp.basic.DropdownEntry
+import top.yukonga.miuix.kmp.basic.DropdownItem
+import top.yukonga.miuix.kmp.basic.Text
+import top.yukonga.miuix.kmp.popup.OverlayDropdownPopup
+import top.yukonga.miuix.kmp.popup.WindowDropdownPopup
+import top.yukonga.miuix.kmp.theme.MiuixTheme
+
+@Composable
+internal fun SettingsDropdownPreference(
+    title: String,
+    items: List<DropdownItem>,
+    selectedIndex: Int,
+    startAction: @Composable () -> Unit,
+    enabled: Boolean = true,
+    useWindow: Boolean = true,
+    onSelectedIndexChange: (Int) -> Unit,
+) {
+    var expanded by rememberSaveable { mutableStateOf(false) }
+    val available = enabled && items.isNotEmpty()
+    LaunchedEffect(available) {
+        if (!available) expanded = false
+    }
+    val haptics = LocalHapticFeedback.current
+    val color = if (available) MiuixTheme.colorScheme.onSurfaceVariantActions
+        else MiuixTheme.colorScheme.disabledOnSurface
+    val entry = DropdownEntry(items.mapIndexed { index, item ->
+        item.copy(selected = index == selectedIndex, onClick = {
+            onSelectedIndexChange(index)
+            item.onClick?.invoke()
+        })
+    })
+    SettingsPreferenceRow(
+        title = title,
+        startAction = startAction,
+        enabled = available,
+        holdDownState = expanded && available,
+        interaction = Modifier.clickable(enabled = available, role = Role.DropdownList) {
+            expanded = !expanded
+            if (expanded) haptics.performHapticFeedback(HapticFeedbackType.ContextClick)
+        },
+    ) {
+        items.getOrNull(selectedIndex)?.text?.let { value ->
+            Text(
+                text = value,
+                modifier = Modifier.weight(1f, fill = false).padding(end = 8.dp),
+                style = MiuixTheme.textStyles.body2,
+                color = color,
+                textAlign = TextAlign.End,
+            )
+        }
+        Row {
+            DropdownArrowEndAction(actionColor = color)
+            if (useWindow) {
+                WindowDropdownPopup(
+                    entry = entry,
+                    show = expanded && available,
+                    onDismiss = { expanded = false },
+                    onDismissFinished = {},
+                    maxHeight = null,
+                    dropdownColors = DropdownDefaults.dropdownColors(),
+                )
+            } else {
+                OverlayDropdownPopup(
+                    entry = entry,
+                    show = expanded && available,
+                    onDismiss = { expanded = false },
+                    onDismissFinished = {},
+                    maxHeight = null,
+                    dropdownColors = DropdownDefaults.dropdownColors(),
+                    renderInRootScaffold = true,
+                )
+            }
+        }
+    }
+}

app/src/main/kotlin/io/github/mangi/eta/ui/components/SettingsItemStyle.kt
@@ -1,0 +1,99 @@
+package io.github.mangi.eta.ui.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.MenuBook
+import androidx.compose.material.icons.rounded.Extension
+import androidx.compose.material.icons.rounded.TheaterComedy
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.luminance
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+import io.github.mangi.eta.ui.app.LocalAppearanceSettings
+import top.yukonga.miuix.kmp.basic.Card
+import top.yukonga.miuix.kmp.basic.HorizontalDivider
+import top.yukonga.miuix.kmp.basic.Icon
+import top.yukonga.miuix.kmp.basic.Text
+import top.yukonga.miuix.kmp.theme.MiuixTheme
+
+internal object SettingsIconColors {
+    val Blue = Color(0xFF0080FF)
+    val Green = StatusSuccess
+    val Orange = Color(0xFFFF7700)
+    val Yellow = StatusWarning
+}
+
+internal object SettingsItemLayout {
+    val SidePadding = 16.dp
+    val IconSize = 24.dp
+    val IconTextGap = 16.dp
+    val ContentStart = SidePadding + IconSize + IconTextGap
+    val RowMinHeight = 52.dp
+}
+
+@Composable
+internal fun SettingsPageTheme(content: @Composable () -> Unit) {
+    val colors = MiuixTheme.colorScheme
+    val appearance = LocalAppearanceSettings.current
+    val pageColors = if (!appearance.monetEnabled && colors.background.luminance() > 0.5f) {
+        colors.copy(background = Color(0xFFF0F1F2))
+    } else {
+        colors
+    }
+    MiuixTheme(colors = pageColors, content = content)
+}
+
+@Composable
+internal fun SettingsGroup(content: @Composable ColumnScope.() -> Unit) {
+    Card(
+        modifier = Modifier.padding(horizontal = SettingsItemLayout.SidePadding).padding(bottom = 16.dp),
+        cornerRadius = 24.dp,
+        content = content,
+    )
+}
+
+@Composable
+internal fun SettingsGroupTitle(text: String) {
+    Text(
+        text = text,
+        style = MiuixTheme.textStyles.subtitle,
+        color = MiuixTheme.colorScheme.onSurfaceVariantSummary,
+        modifier = Modifier.padding(start = 32.dp, end = 32.dp, top = 4.dp, bottom = 8.dp),
+    )
+}
+
+@Composable
+internal fun SettingsPreferenceIcon(
+    icon: ImageVector,
+    tint: Color,
+    enabled: Boolean = true,
+) {
+    // 满幅轮廓稍作光学校正，但所有图标都占相同宽度，保证正文和分割线对齐。
+    val glyphSize = when (icon) {
+        Icons.Rounded.Extension, Icons.Rounded.TheaterComedy, Icons.AutoMirrored.Rounded.MenuBook -> 22.dp
+        else -> SettingsItemLayout.IconSize
+    }
+    Box(Modifier.size(SettingsItemLayout.IconSize), contentAlignment = Alignment.Center) {
+        Icon(
+            imageVector = icon,
+            contentDescription = null,
+            modifier = Modifier.size(glyphSize),
+            tint = if (enabled) tint else MiuixTheme.colorScheme.disabledOnSurface,
+        )
+    }
+}
+
+@Composable
+internal fun SettingsItemDivider() {
+    HorizontalDivider(
+        modifier = Modifier.padding(start = SettingsItemLayout.ContentStart, end = SettingsItemLayout.SidePadding),
+        thickness = 0.33.dp,
+        color = MiuixTheme.colorScheme.onBackground.copy(alpha = 0.1f),
+    )
+}

app/src/main/kotlin/io/github/mangi/eta/ui/components/SettingsPreferenceRow.kt
@@ -1,0 +1,159 @@
+package io.github.mangi.eta.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.selection.toggleable
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.semantics.clearAndSetSemantics
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
+import top.yukonga.miuix.kmp.basic.Icon
+import top.yukonga.miuix.kmp.basic.Switch
+import top.yukonga.miuix.kmp.basic.Text
+import top.yukonga.miuix.kmp.icon.MiuixIcons
+import top.yukonga.miuix.kmp.icon.basic.ArrowRight
+import top.yukonga.miuix.kmp.theme.MiuixTheme
+
+@Composable
+internal fun SettingsArrowPreference(
+    title: String,
+    summary: String? = null,
+    startAction: @Composable () -> Unit,
+    endActions: @Composable RowScope.() -> Unit = {},
+    enabled: Boolean = true,
+    holdDownState: Boolean = false,
+    onClick: () -> Unit,
+) {
+    SettingsPreferenceRow(
+        title = title,
+        summary = summary,
+        startAction = startAction,
+        enabled = enabled,
+        holdDownState = holdDownState,
+        interaction = Modifier.clickable(enabled = enabled, onClick = onClick),
+    ) {
+        Row(modifier = Modifier.weight(1f, fill = false), content = endActions)
+        Spacer(Modifier.width(8.dp))
+        val direction = LocalLayoutDirection.current
+        Icon(
+            imageVector = MiuixIcons.Basic.ArrowRight,
+            contentDescription = null,
+            modifier = Modifier.size(8.dp, 14.dp).graphicsLayer {
+                scaleX = if (direction == LayoutDirection.Rtl) -1f else 1f
+            },
+            tint = if (enabled) MiuixTheme.colorScheme.onBackground.copy(alpha = 0.3f)
+                else MiuixTheme.colorScheme.disabledOnSurface,
+        )
+    }
+}
+
+@Composable
+internal fun SettingsSwitchPreference(
+    title: String,
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
+    summary: String? = null,
+    startAction: @Composable () -> Unit,
+    enabled: Boolean = true,
+) {
+    SettingsPreferenceRow(
+        title = title,
+        summary = summary,
+        startAction = startAction,
+        enabled = enabled,
+        interaction = Modifier.toggleable(
+            value = checked,
+            enabled = enabled,
+            role = Role.Switch,
+            onValueChange = onCheckedChange,
+        ),
+    ) {
+        Switch(
+            checked = checked,
+            onCheckedChange = onCheckedChange,
+            enabled = enabled,
+            // 整行提供无障碍状态，开关保留点击和拖动；缩放绘制不缩小行的触摸区域。
+            modifier = Modifier.clearAndSetSemantics {}.layout { measurable, constraints ->
+                val placeable = measurable.measure(constraints.copy(minWidth = 0, minHeight = 0))
+                val width = 44.dp.roundToPx()
+                val height = 24.dp.roundToPx()
+                layout(width, height) {
+                    placeable.placeWithLayer((width - placeable.width) / 2, (height - placeable.height) / 2) {
+                        scaleX = width.toFloat() / placeable.width
+                        scaleY = height.toFloat() / placeable.height
+                    }
+                }
+            },
+        )
+    }
+}
+
+@Composable
+internal fun SettingsPreferenceRow(
+    title: String,
+    startAction: @Composable () -> Unit,
+    interaction: Modifier,
+    summary: String? = null,
+    enabled: Boolean = true,
+    holdDownState: Boolean = false,
+    endActions: @Composable RowScope.() -> Unit,
+) {
+    val colors = MiuixTheme.colorScheme
+    // 通用设置组件的最小高度固定为 56dp；首页单独布局，长文本仍可自然增高。
+    BoxWithConstraints(
+        modifier = Modifier.fillMaxWidth()
+            .background(if (holdDownState) colors.onBackground.copy(alpha = 0.06f) else Color.Transparent)
+            .then(interaction),
+    ) {
+        val actionMaxWidth = (maxWidth - SettingsItemLayout.ContentStart - SettingsItemLayout.SidePadding) * 0.45f
+        Row(
+            modifier = Modifier.fillMaxWidth()
+                .heightIn(min = SettingsItemLayout.RowMinHeight)
+                .padding(horizontal = SettingsItemLayout.SidePadding, vertical = 12.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            startAction()
+            Spacer(Modifier.width(SettingsItemLayout.IconTextGap))
+            Column(Modifier.weight(1f)) {
+                Text(
+                    text = title,
+                    style = MiuixTheme.textStyles.body1,
+                    fontWeight = FontWeight.Medium,
+                    color = if (enabled) colors.onBackground else colors.disabledOnSurface,
+                )
+                if (summary != null) {
+                    Text(
+                        text = summary,
+                        style = MiuixTheme.textStyles.body2,
+                        color = if (enabled) colors.onSurfaceVariantSummary else colors.disabledOnSurface,
+                    )
+                }
+            }
+            Spacer(Modifier.width(12.dp))
+            Row(
+                modifier = Modifier.widthIn(max = actionMaxWidth.coerceAtLeast(44.dp)),
+                verticalAlignment = Alignment.CenterVertically,
+                content = endActions,
+            )
+        }
+    }
+}

Asagnc/Eta-as

(cherry picked from commit e351630cf75fd3e1ef3d57bb97f1c46172eec573)

app/src/main/kotlin/io/github/asagnc/sta/ui/components/SettingsDropdownPreference.kt
@@ -1,0 +1,94 @@
+package io.github.asagnc.sta.ui.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.ui.platform.LocalHapticFeedback
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import top.yukonga.miuix.kmp.basic.DropdownArrowEndAction
+import top.yukonga.miuix.kmp.basic.DropdownDefaults
+import top.yukonga.miuix.kmp.basic.DropdownEntry
+import top.yukonga.miuix.kmp.basic.DropdownItem
+import top.yukonga.miuix.kmp.basic.Text
+import top.yukonga.miuix.kmp.popup.OverlayDropdownPopup
+import top.yukonga.miuix.kmp.popup.WindowDropdownPopup
+import top.yukonga.miuix.kmp.theme.MiuixTheme
+
+@Composable
+internal fun SettingsDropdownPreference(
+    title: String,
+    items: List<DropdownItem>,
+    selectedIndex: Int,
+    startAction: @Composable () -> Unit,
+    enabled: Boolean = true,
+    useWindow: Boolean = true,
+    onSelectedIndexChange: (Int) -> Unit,
+) {
+    var expanded by rememberSaveable { mutableStateOf(false) }
+    val available = enabled && items.isNotEmpty()
+    LaunchedEffect(available) {
+        if (!available) expanded = false
+    }
+    val haptics = LocalHapticFeedback.current
+    val color = if (available) MiuixTheme.colorScheme.onSurfaceVariantActions
+        else MiuixTheme.colorScheme.disabledOnSurface
+    val entry = DropdownEntry(items.mapIndexed { index, item ->
+        item.copy(selected = index == selectedIndex, onClick = {
+            onSelectedIndexChange(index)
+            item.onClick?.invoke()
+        })
+    })
+    SettingsPreferenceRow(
+        title = title,
+        startAction = startAction,
+        enabled = available,
+        holdDownState = expanded && available,
+        interaction = Modifier.clickable(enabled = available, role = Role.DropdownList) {
+            expanded = !expanded
+            if (expanded) haptics.performHapticFeedback(HapticFeedbackType.ContextClick)
+        },
+    ) {
+        items.getOrNull(selectedIndex)?.text?.let { value ->
+            Text(
+                text = value,
+                modifier = Modifier.weight(1f, fill = false).padding(end = 8.dp),
+                style = MiuixTheme.textStyles.body2,
+                color = color,
+                textAlign = TextAlign.End,
+            )
+        }
+        Row {
+            DropdownArrowEndAction(actionColor = color)
+            if (useWindow) {
+                WindowDropdownPopup(
+                    entry = entry,
+                    show = expanded && available,
+                    onDismiss = { expanded = false },
+                    onDismissFinished = {},
+                    maxHeight = null,
+                    dropdownColors = DropdownDefaults.dropdownColors(),
+                )
+            } else {
+                OverlayDropdownPopup(
+                    entry = entry,
+                    show = expanded && available,
+                    onDismiss = { expanded = false },
+                    onDismissFinished = {},
+                    maxHeight = null,
+                    dropdownColors = DropdownDefaults.dropdownColors(),
+                    renderInRootScaffold = true,
+                )
+            }
+        }
+    }
+}

app/src/main/kotlin/io/github/asagnc/sta/ui/components/SettingsItemStyle.kt
@@ -1,0 +1,99 @@
+package io.github.asagnc.sta.ui.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.MenuBook
+import androidx.compose.material.icons.rounded.Extension
+import androidx.compose.material.icons.rounded.TheaterComedy
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.luminance
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+import io.github.asagnc.sta.ui.app.LocalAppearanceSettings
+import top.yukonga.miuix.kmp.basic.Card
+import top.yukonga.miuix.kmp.basic.HorizontalDivider
+import top.yukonga.miuix.kmp.basic.Icon
+import top.yukonga.miuix.kmp.basic.Text
+import top.yukonga.miuix.kmp.theme.MiuixTheme
+
+internal object SettingsIconColors {
+    val Blue = Color(0xFF0080FF)
+    val Green = StatusSuccess
+    val Orange = Color(0xFFFF7700)
+    val Yellow = StatusWarning
+}
+
+internal object SettingsItemLayout {
+    val SidePadding = 16.dp
+    val IconSize = 24.dp
+    val IconTextGap = 16.dp
+    val ContentStart = SidePadding + IconSize + IconTextGap
+    val RowMinHeight = 52.dp
+}
+
+@Composable
+internal fun SettingsPageTheme(content: @Composable () -> Unit) {
+    val colors = MiuixTheme.colorScheme
+    val appearance = LocalAppearanceSettings.current
+    val pageColors = if (!appearance.monetEnabled && colors.background.luminance() > 0.5f) {
+        colors.copy(background = Color(0xFFF0F1F2))
+    } else {
+        colors
+    }
+    MiuixTheme(colors = pageColors, content = content)
+}
+
+@Composable
+internal fun SettingsGroup(content: @Composable ColumnScope.() -> Unit) {
+    Card(
+        modifier = Modifier.padding(horizontal = SettingsItemLayout.SidePadding).padding(bottom = 16.dp),
+        cornerRadius = 24.dp,
+        content = content,
+    )
+}
+
+@Composable
+internal fun SettingsGroupTitle(text: String) {
+    Text(
+        text = text,
+        style = MiuixTheme.textStyles.subtitle,
+        color = MiuixTheme.colorScheme.onSurfaceVariantSummary,
+        modifier = Modifier.padding(start = 32.dp, end = 32.dp, top = 4.dp, bottom = 8.dp),
+    )
+}
+
+@Composable
+internal fun SettingsPreferenceIcon(
+    icon: ImageVector,
+    tint: Color,
+    enabled: Boolean = true,
+) {
+    // 满幅轮廓稍作光学校正，但所有图标都占相同宽度，保证正文和分割线对齐。
+    val glyphSize = when (icon) {
+        Icons.Rounded.Extension, Icons.Rounded.TheaterComedy, Icons.AutoMirrored.Rounded.MenuBook -> 22.dp
+        else -> SettingsItemLayout.IconSize
+    }
+    Box(Modifier.size(SettingsItemLayout.IconSize), contentAlignment = Alignment.Center) {
+        Icon(
+            imageVector = icon,
+            contentDescription = null,
+            modifier = Modifier.size(glyphSize),
+            tint = if (enabled) tint else MiuixTheme.colorScheme.disabledOnSurface,
+        )
+    }
+}
+
+@Composable
+internal fun SettingsItemDivider() {
+    HorizontalDivider(
+        modifier = Modifier.padding(start = SettingsItemLayout.ContentStart, end = SettingsItemLayout.SidePadding),
+        thickness = 0.33.dp,
+        color = MiuixTheme.colorScheme.onBackground.copy(alpha = 0.1f),
+    )
+}

app/src/main/kotlin/io/github/asagnc/sta/ui/components/SettingsPreferenceRow.kt
@@ -1,0 +1,159 @@
+package io.github.asagnc.sta.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.selection.toggleable
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.semantics.clearAndSetSemantics
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
+import top.yukonga.miuix.kmp.basic.Icon
+import top.yukonga.miuix.kmp.basic.Switch
+import top.yukonga.miuix.kmp.basic.Text
+import top.yukonga.miuix.kmp.icon.MiuixIcons
+import top.yukonga.miuix.kmp.icon.basic.ArrowRight
+import top.yukonga.miuix.kmp.theme.MiuixTheme
+
+@Composable
+internal fun SettingsArrowPreference(
+    title: String,
+    summary: String? = null,
+    startAction: @Composable () -> Unit,
+    endActions: @Composable RowScope.() -> Unit = {},
+    enabled: Boolean = true,
+    holdDownState: Boolean = false,
+    onClick: () -> Unit,
+) {
+    SettingsPreferenceRow(
+        title = title,
+        summary = summary,
+        startAction = startAction,
+        enabled = enabled,
+        holdDownState = holdDownState,
+        interaction = Modifier.clickable(enabled = enabled, onClick = onClick),
+    ) {
+        Row(modifier = Modifier.weight(1f, fill = false), content = endActions)
+        Spacer(Modifier.width(8.dp))
+        val direction = LocalLayoutDirection.current
+        Icon(
+            imageVector = MiuixIcons.Basic.ArrowRight,
+            contentDescription = null,
+            modifier = Modifier.size(8.dp, 14.dp).graphicsLayer {
+                scaleX = if (direction == LayoutDirection.Rtl) -1f else 1f
+            },
+            tint = if (enabled) MiuixTheme.colorScheme.onBackground.copy(alpha = 0.3f)
+                else MiuixTheme.colorScheme.disabledOnSurface,
+        )
+    }
+}
+
+@Composable
+internal fun SettingsSwitchPreference(
+    title: String,
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
+    summary: String? = null,
+    startAction: @Composable () -> Unit,
+    enabled: Boolean = true,
+) {
+    SettingsPreferenceRow(
+        title = title,
+        summary = summary,
+        startAction = startAction,
+        enabled = enabled,
+        interaction = Modifier.toggleable(
+            value = checked,
+            enabled = enabled,
+            role = Role.Switch,
+            onValueChange = onCheckedChange,
+        ),
+    ) {
+        Switch(
+            checked = checked,
+            onCheckedChange = onCheckedChange,
+            enabled = enabled,
+            // 整行提供无障碍状态，开关保留点击和拖动；缩放绘制不缩小行的触摸区域。
+            modifier = Modifier.clearAndSetSemantics {}.layout { measurable, constraints ->
+                val placeable = measurable.measure(constraints.copy(minWidth = 0, minHeight = 0))
+                val width = 44.dp.roundToPx()
+                val height = 24.dp.roundToPx()
+                layout(width, height) {
+                    placeable.placeWithLayer((width - placeable.width) / 2, (height - placeable.height) / 2) {
+                        scaleX = width.toFloat() / placeable.width
+                        scaleY = height.toFloat() / placeable.height
+                    }
+                }
+            },
+        )
+    }
+}
+
+@Composable
+internal fun SettingsPreferenceRow(
+    title: String,
+    startAction: @Composable () -> Unit,
+    interaction: Modifier,
+    summary: String? = null,
+    enabled: Boolean = true,
+    holdDownState: Boolean = false,
+    endActions: @Composable RowScope.() -> Unit,
+) {
+    val colors = MiuixTheme.colorScheme
+    // 通用设置组件的最小高度固定为 56dp；首页单独布局，长文本仍可自然增高。
+    BoxWithConstraints(
+        modifier = Modifier.fillMaxWidth()
+            .background(if (holdDownState) colors.onBackground.copy(alpha = 0.06f) else Color.Transparent)
+            .then(interaction),
+    ) {
+        val actionMaxWidth = (maxWidth - SettingsItemLayout.ContentStart - SettingsItemLayout.SidePadding) * 0.45f
+        Row(
+            modifier = Modifier.fillMaxWidth()
+                .heightIn(min = SettingsItemLayout.RowMinHeight)
+                .padding(horizontal = SettingsItemLayout.SidePadding, vertical = 12.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            startAction()
+            Spacer(Modifier.width(SettingsItemLayout.IconTextGap))
+            Column(Modifier.weight(1f)) {
+                Text(
+                    text = title,
+                    style = MiuixTheme.textStyles.body1,
+                    fontWeight = FontWeight.Medium,
+                    color = if (enabled) colors.onBackground else colors.disabledOnSurface,
+                )
+                if (summary != null) {
+                    Text(
+                        text = summary,
+                        style = MiuixTheme.textStyles.body2,
+                        color = if (enabled) colors.onSurfaceVariantSummary else colors.disabledOnSurface,
+                    )
+                }
+            }
+            Spacer(Modifier.width(12.dp))
+            Row(
+                modifier = Modifier.widthIn(max = actionMaxWidth.coerceAtLeast(44.dp)),
+                verticalAlignment = Alignment.CenterVertically,
+                content = endActions,
+            )
+        }
+    }
+}